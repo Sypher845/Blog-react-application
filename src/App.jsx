777a7b24c97fe756f1c56b228d601055d47ec6b6
Sypher845 <suyashpatil845@gmail.com>
2025-03-13 18:41:16 +0530

    env

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import conf from './conf/conf';
+
+
+function App() {
+  console.log(conf.appwriteURL);
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
